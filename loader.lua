f31429a3164e787321c2b2a6bbab2a64dab3b4d5
z4xi <36055611+z4xi@users.noreply.github.com>
2022-08-17 21:47:10 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/titanium/main/main.lua"))()
+if not _G.Welcomed then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/titanium/main/welcome.lua"))()
+elseif _G.Welcomed = true then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/titanium/main/main.lua"))()
+end
