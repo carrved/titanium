5e54a7018985983fea30436e8aacebe30e12e599
z4xi <36055611+z4xi@users.noreply.github.com>
2022-08-18 21:24:44 -0500

    Delete loader.lua

--- loader.lua
+++
@@ -1,5 +0,0 @@
-if not _G.Welcomed then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/titanium/main/welcome.lua"))()
-elseif _G.Welcomed = true then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/titanium/main/main.lua"))()
-end
